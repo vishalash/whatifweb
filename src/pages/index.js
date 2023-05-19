42838dc105190626826a727f39b159fa5914aaf5
vishalash <vishalashx2@gmail.com>
2023-05-19 11:21:59 +0530

    added head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [siteName, setSiteName] = useState("github.com");
   const [htmlData, setHtmlData] = useState("");
+  const iframe = useRef();
 
   async function fetchData() {
+    iframe.current.classList.toggle("blur");
     let response = await fetch(`/api/fetch?siteName=${siteName}`);
     const data = await response.text();
     setHtmlData(data);
+    iframe.current.classList.toggle("blur");
   }
 
   const changeHandler = (event) => {
@@ -20,31 +24,40 @@ export default function Home() {
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   return (
-    <div className="container mx-auto py-8 bg-yellow-200">
-      <h1 className="text-3xl font-bold mb-4 text-center text-black">
-        Enter a domain you want to visit
-      </h1>
-      <div className="flex items-center justify-center mb-4">
-        <input
-          placeholder="github.com"
-          className="border border-black rounded py-2 px-4 mr-2 w-1/2 focus:outline-none"
-          value={siteName}
-          onChange={changeHandler}
-        />
-        <button
-          className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
-          onClick={handleButtonClick}
-        >
-          Fetch
-        </button>
+    <>
+      <Head>
+        <title>What If Web - Go to a domain for fun</title>
+      </Head>
+      <div className="container mx-auto py-8 bg-yellow-200">
+        <h1 className="text-3xl font-bold mb-4 text-center text-black">
+          Enter a domain you want to visit
+        </h1>
+        <div className="flex items-center justify-center mb-4">
+          <input
+            placeholder="github.com"
+            className="border border-black rounded py-2 px-4 mr-2 w-1/2 focus:outline-none"
+            value={siteName}
+            onChange={changeHandler}
+          />
+          <button
+            className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+            onClick={handleButtonClick}
+          >
+            Fetch
+          </button>
+        </div>
+        <hr className="mb-4" />
+        <div className="p-4 bg-black rounded shadow-lg">
+          <iframe
+            className="w-full h-screen"
+            ref={iframe}
+            srcDoc={htmlData}
+          ></iframe>
+        </div>
       </div>
-      <hr className="mb-4" />
-      <div className="p-4 bg-black rounded shadow-lg">
-        <iframe className="w-full h-screen" srcDoc={htmlData}></iframe>
-      </div>
-    </div>
+    </>
   );
 }
